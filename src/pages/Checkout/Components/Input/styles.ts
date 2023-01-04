1fc0a6242d385083b44ff57f717bd23d646e7007
Gean Lucas <gean.lfpsa@gmail.com>
2023-01-03 22:02:30 -0300

    feat: input placeholder color

--- src/pages/Checkout/Components/Input/styles.ts
+++ src/pages/Checkout/Components/Input/styles.ts
@@ -3,11 +3,14 @@ import styled from 'styled-components'
 export const InputContainer = styled.input`
   width: 100%;
   background: ${(props) => props.theme['base-input']};
-  color: ${(props) => props.theme['base-label']};
   border: 0;
   border-radius: 4px;
   padding: 0.75rem;
 
   font-size: 0.875rem;
   line-height: 130%;
+
+  ::placeholder {
+    color: ${(props) => props.theme['base-label']};
+  }
 `
